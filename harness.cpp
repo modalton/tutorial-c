10860f4d8a1d586604c0081031a7593dd5accff3
Everest Munro-Zeisberger <evmunro1@gmail.com>
2020-12-08 14:51:58 -0800

    Update for new fuzzbuzz.yaml

--- harness.cpp
+++ harness.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 // Simple fuzz target for BrokenMethod().
-extern "C" int FuzzerEntrypoint(const uint8_t *data, size_t size) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::string str(reinterpret_cast<const char *>(data), size);
   BrokenMethod(str);  // Disregard the output.
   return 0;
